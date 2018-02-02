410381454fac3bdd7374ec78595d4d075a8e5cd4
monishathakur <monishathakur@ymail.com>
2018-02-01 21:17:14 -0600

    [R+M][trainingTeam] feat(challenge): challenge solutions

--- challenge-area/challenge.js
+++ challenge-area/challenge.js
@@ -1,1 +1,62 @@
-//Good luck! Maybe start by making that fetch request ;)
+let fetchedResults;
+
+fetch('https://opentdb.com/api.php?amount=10&category=18').then(res => res.json())
+    .then(response => {
+        fetchedResults = response.results;
+        console.log('original results', fetchedResults);
+        //1
+        console.log(replaceQuotes([...fetchedResults]), 'Replaced quotes');
+        //2
+        console.log(filterQuestionsByDifficulty([...fetchedResults], 'easy'), 'Easy Questions');
+        //3
+        console.log(sortByProperty([...fetchedResults], 'difficulty'), 'Sort by difficulty');
+        //4
+        console.log(difficultyCounter([...fetchedResults]), 'Difficulty counter');
+        //5
+        console.log(hasScienceComputers([...fetchedResults]), 'Has Science: Computers');
+        //6
+        console.log(filterAndSort([...fetchedResults]), 'Filterd by medium and sorted on type');
+    });
+
+
+const replaceQuotes = results => {
+    return results.map(obj => {
+        return obj.question.replace(/&quot;/g, '"');
+    });
+};
+
+const filterQuestionsByDifficulty = (results, difficultyLevel) => {
+    return results.filter((obj) => obj.difficulty === difficultyLevel)
+};
+
+const cmpfn  = prop => {
+    return (a,b) => a[prop] > b[prop];
+};
+
+//string sort by property name
+const sortByProperty = (results, prop) => {
+    return results.sort(cmpfn(prop));
+};
+
+//tried doing this with reducer but couldn't succeed.
+
+const difficultyCounter = results => {
+    return {
+        easy: filterQuestionsByDifficulty(results, 'easy').length,
+        medium: filterQuestionsByDifficulty(results, 'medium').length,
+        hard: filterQuestionsByDifficulty(results, 'hard').length
+    };
+};
+
+
+const hasScienceComputers = results => {
+    return results.every(item => item.category === 'Science: Computers');
+};
+
+const filterAndSort = results => {
+    const mediumDifficultyQuestions = filterQuestionsByDifficulty(results, 'medium');
+
+    return sortByProperty(mediumDifficultyQuestions, 'type');
+
+};
+
